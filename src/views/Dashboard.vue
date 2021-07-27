f2ea286ee731ba51e515c043a3d5ca3a014146a0
nagarjun063 <you@example.com>
2021-07-27 08:57:41 +0530

    db conection established

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,340 +1,271 @@
 <template>
   <div>
-    <h1 class="subheading grey--text pl=4">DASHBOARD</h1>
-    <v-container pt-6 pb-2 pl-4 pr-0>
-      <v-card class="mx-auto" max-width="850" min-height="1200">
-        <v-container pt-6 pb-2 pl-12 pr-4>
-          <v-row>
-            <v-col :cols="6">
-              <v-card class="mx-auto" min-width="350" outlined>
-                <v-subheader inset><h2>ANALYTICS 1</h2></v-subheader>
-                <div class="card-body text-center">
-                  <VGauge
-                    :maxValue="100"
-                    :minValue="0"
-                    :value="gaugeValue"
-                    width="300px"
-                    :unit="gaugeUnit"
-                    top
-                  />
-                </div>
-              </v-card>
-            </v-col>
-            <v-spacer> </v-spacer>
-            <v-col :cols="6">
-              <v-card class="mt-8 mx-auto" max-width="350">
-                <v-sheet
-                  class="v-sheet--offset mx-auto"
-                  color="cyan"
-                  elevation="12"
-                  max-width="calc(100% - 32px)"
-                >
-                  <v-sparkline
-                    :labels="labels2"
-                    :value="value"
-                    color="white"
-                    line-width="2"
-                    padding="16"
-                  ></v-sparkline>
-                </v-sheet>
-
-                <v-card-text class="pt-0">
-                  <div class="title font-weight-light mb-2">
-                    User Registrations
-                  </div>
-                  <div class="subheading font-weight-light grey--text">
-                    Last Campaign Performance
-                  </div>
-                  <v-divider class="my-2"></v-divider>
-                  <v-icon class="mr-2" small> mdi-clock </v-icon>
-                  <span class="caption grey--text font-weight-light"
-                    >last registration 26 minutes ago</span
-                  >
-                </v-card-text>
-              </v-card>
-            </v-col>
+      <v-col class="scrollable">
+        <v-card class="ma-16 pa-8">
+          <v-layout row justify-space-between>
+            <h2 class="heading black--text pl-2">Engagement</h2>
+            <div style="max-width: 25%" class="pr-6">
+              <v-select
+                :items="chartdatesoptions"
+                v-model="chartdates"
+                @change="date(chartdates)"
+              ></v-select>
+            </div>
+          </v-layout>
+          <v-row class="my-8 justify-space-around">
+            <v-card class="px-6">
+              <v-card-title class="headline mb-1 justify-center">
+                <v-icon class="pr-2" color="orange" large
+                  >mdi-clock-time-nine</v-icon
+                >Total Time Spent</v-card-title
+              >
+              <v-divider class="mx-2"></v-divider>
+              <v-card-title class="headline mb-1 justify-center"
+                >{{ totaltime }} seconds</v-card-title
+              >
+            </v-card>
+            <v-card class="px-6">
+              <v-card-title class="headline mb-1 justify-center">
+                <v-icon class="pr-2" color="orange" large>mdi-timer</v-icon
+                >Average Time Spent</v-card-title
+              >
+              <v-divider class="mx-2"></v-divider>
+              <v-card-title class="headline mb-1 justify-center"
+                >{{ averagetime }} seconds</v-card-title
+              >
+            </v-card>
           </v-row>
-
-          <v-row>
-            <v-col :cols="6">
-              <v-card class="mx-auto mt-6" min-width="350" outlined>
-                <v-subheader inset><h2>ANALYTICS 3</h2></v-subheader>
-
-                <div class="card-body">
-                  <HorizontalBarChart
-                    v-bind:chartdata="chartData_Row2_Chart1"
-                    v-bind:options="chartOptions_Row2_Chart1"
-                  />
-                </div>
-              </v-card>
-            </v-col>
-
-            <v-spacer></v-spacer>
-            <v-col :cols="6">
-              <v-card class="mx-auto mt-6" max-width="450" outlined>
-                <v-subheader inset><h2>ANALYTICS 4</h2></v-subheader>
-                <div class="card-body">
-                  <MixedChart
-                    v-bind:chartdata="chartData_Row2_Chart2"
-                    v-bind:options="chartOptions_Row2_Chart2"
-                  />
-                </div>
-              </v-card>
-            </v-col>
+          <v-row class="my-16 justify-space-around">
+            <v-card class="px-6">
+              <v-card-title class="headline mb-1 justify-center">
+                <v-icon class="pr-2" color="orange" large>mdi-magnify</v-icon
+                >No of clicked</v-card-title
+              >
+              <v-divider class="mx-2"></v-divider>
+              <v-card-title class="headline mb-1 justify-center">{{
+                nooffaqclicks
+              }}</v-card-title>
+            </v-card>
+            <v-card class="px-6">
+              <v-card-title class="headline mb-1 justify-center">
+                <v-icon class="pr-2" color="orange" large
+                  >mdi-view-dashboard</v-icon
+                >Average clicked</v-card-title
+              >
+              <v-divider class="mx-2"></v-divider>
+              <v-card-title class="headline mb-1 justify-center">{{
+                averageclicks
+              }}</v-card-title>
+            </v-card>
           </v-row>
-
-          <!--  -->
-          <!-- Analytics Row 3 -->
-          <!--  -->
-
-          <v-row>
-            <v-col :cols="6">
-              <v-card class="mx-auto mt-6" max-width="350">
-                <v-subheader inset><h2>KNOWLEDGE BASE</h2></v-subheader>
-
-                <v-toolbar flat color="transparent">
-                  <v-btn icon>
-                    <v-icon>mdi-arrow-left</v-icon>
-                  </v-btn>
-
-                  <v-text-field
-                    v-model="search"
-                    append-icon="mdi-magnify"
-                    label="Search News"
-                    single-line
-                  ></v-text-field>
-                </v-toolbar>
-
-                <v-card-text class="py-0">
-                  <v-chip
-                    v-for="(keyword, i) in keywords"
-                    :key="i"
-                    class="mr-2"
-                  >
-                    {{ keyword }}
-                  </v-chip>
-                </v-card-text>
-
-                <v-list three-line>
-                  <v-list-item
-                    v-for="(item, i) in searching"
-                    :key="i"
-                    ripple
-                    @click="() => {}"
-                  >
-                    <v-img
-                      :src="item.image"
-                      class="mr-4"
-                      max-width="64"
-                      min-width="64"
-                    ></v-img>
-
-                    <v-list-item-content>
-                      <span
-                        class="text-uppercase font-weight-regular caption"
-                        v-text="item.category"
-                      ></span>
-
-                      <div v-text="item.title"></div>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-col>
-
-            <v-col :cols="6">
-              <v-card max-width="600" class="mx-auto mt-8">
-                <v-list subheader two-line>
-                  <v-subheader inset><h2>REPORTS</h2></v-subheader>
-
-                  <v-list-item v-for="folder in folders" :key="folder.title">
-                    <v-list-item-avatar>
-                      <v-icon class="grey lighten-1" dark> mdi-folder </v-icon>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title
-                        v-text="folder.title"
-                      ></v-list-item-title>
-                    </v-list-item-content>
-
-                    <v-list-item-action>
-                      <v-btn icon>
-                        <v-icon color="grey lighten-1">mdi-download</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-col>
+          <v-card class="my-16">
+            <v-card-title>
+              FAQS
+              <v-spacer></v-spacer>
+              <v-text-field
+                v-model="search_faq"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+              ></v-text-field>
+            </v-card-title>
+            <v-data-table
+              :headers="faq_headers"
+              :items="faq_names"
+              :search="search_faq"
+            >
+            </v-data-table>
+          </v-card>
+          <v-row class="mt-16 mb-4 justify-space-around">
+            <v-card class="px-6">
+              <v-card-title class="headline mb-1 justify-center">
+                <v-icon class="pr-2" color="orange" large>mdi-magnify</v-icon
+                >Agents Contacted</v-card-title
+              >
+              <v-divider class="mx-2"></v-divider>
+              <v-card-title class="headline mb-1 justify-center">{{
+                agents
+              }}</v-card-title>
+            </v-card>
+            <v-card class="px-6">
+              <v-card-title class="headline mb-1 justify-center">
+                <v-icon class="pr-2" color="orange" large>mdi-magnify</v-icon
+                >Website Views</v-card-title
+              >
+              <v-divider class="mx-2"></v-divider>
+              <v-card-title class="headline mb-1 justify-center">{{
+                website
+              }}</v-card-title>
+            </v-card>
           </v-row>
-        </v-container>
-      </v-card>
-    </v-container>
+        </v-card>
+      </v-col>
   </div>
 </template>
 
 <script>
-// import BarChart from "@/components/BarChart";
-import HorizontalBarChart from "@/components/HorizontalBarChart";
-import MixedChart from "@/components/MixedChart";
-import VGauge from "vgauge";
-
+import moment from 'moment';
+import $ from "jquery";
 export default {
   name: "dashboard",
   components: {
-    // BarChart,
-    HorizontalBarChart,
-    MixedChart,
-    VGauge,
   },
   data() {
     return {
-      folders: [
+      totaltime: 0,
+      averagetime: 0,
+      nooffaqclicks: 0,
+      averageclicks: 0,
+      agents: 0,
+      website: 0,
+      faqs: [],
+      windowWidth: window.innerWidth,
+      txt: "",
+      chartdates: "Total",
+      chartdatesoptions: ["Daily", "Weekly", "Monthly", "Yearly", "Total"],
+      faq_headers: [
         {
-          subtitle: "Jan 9, 2014",
-          title: "TODAY",
+          text: "Buttons",
+          align: "start",
+          sortable: false,
+          value: "name",
         },
-        {
-          subtitle: "Jan 17, 2014",
-          title: "THIS WEEK",
-        },
-        {
-          subtitle: "Jan 28, 2014",
-          title: "THIS MONTH",
-        },
-        {
-          subtitle: "Jan 28, 2014",
-          title: "THIS YEAR",
-        },
-        {
-          subtitle: "Jan 28, 2014",
-          title: "REACH",
-        },
-        {
-          subtitle: "Jan 28, 2014",
-          title: "CAMPAIGNS",
-        },
-        {
-          subtitle: "Jan 28, 2014",
-          title: "DAILER",
-        },
+        { text: "NO of Clicks", value: "clicks" },
       ],
-      // Chart
-      chartData_Row2_Chart1: {
-        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-
-        datasets: [
-          {
-            label: "Population (millions)",
-            backgroundColor: [
-              "#3e95cd",
-              "#8e5ea2",
-              "#3cba9f",
-              "#e8c3b9",
-              "#c45850",
-            ],
-
-            data: [2478, 5267, 734, 784, 433],
-          },
-        ],
-      },
-      labels2: ["12am", "3am", "6am", "9am", "12pm", "3pm", "6pm", "9pm"],
-      value: [200, 675, 410, 390, 310, 460, 250, 240],
-      chartOptions_Row2_Chart1: {
-        legend: { display: false },
-        title: {
-          display: true,
-          text: "Predicted world population (millions) in 2050",
-        },
-      },
-
-      // Chart
-      chartData_Row2_Chart2: {
-        labels: ["1900", "1950", "1999", "2050"],
-        datasets: [
-          {
-            label: "Europe",
-            type: "line",
-            borderColor: "#8e5ea2",
-            data: [408, 547, 675, 734],
-            fill: false,
-          },
-          {
-            label: "Africa",
-            type: "line",
-            borderColor: "#3e95cd",
-            data: [133, 221, 783, 2478],
-            fill: false,
-          },
-          {
-            label: "Europe",
-            type: "bar",
-            backgroundColor: "rgba(0,0,0,0.2)",
-            data: [408, 547, 675, 734],
-          },
-          {
-            label: "Africa",
-            type: "bar",
-            backgroundColor: "rgba(0,0,0,0.2)",
-            backgroundColorHover: "#3e95cd",
-            data: [133, 221, 783, 2478],
-          },
-        ],
-      },
-      chartOptions_Row2_Chart2: {
-        title: {
-          display: true,
-          text: "Population growth (millions): Europe & Africa",
-        },
-        legend: { display: false },
-      },
-
-      items: [
-        {
-          image:
-            "https://cdn-images-1.medium.com/max/1024/1*9C9hLji68wV373tk8okLYA.jpeg",
-          title: "TBI’s 5 Best: SF Mocktails to Finish Dry January Strong",
-          category: "Travel",
-          keyword: "Drinks",
-        },
-        {
-          image:
-            "https://cdn-images-1.medium.com/max/1024/1*BBNtYUieAqHoXKjiJ2mMjQ.png",
-          title:
-            "PWAs on iOS 12.2 beta: the good, the bad, and the “not sure yet if good”",
-          category: "Technology",
-          keyword: "Phones",
-        },
-        {
-          image:
-            "https://cdn-images-1.medium.com/max/1024/1*rTEtei1UEmNqbq6evRsExw.jpeg",
-          title: "How to Get Media Mentions for Your Business",
-          category: "Media",
-          keyword: "Social",
-        },
-        {
-          image:
-            "https://cdn-images-1.medium.com/max/1024/1*FD2nkJewVeQnGf0ommQfrw.jpeg",
-          title:
-            "The Pitfalls Of Outsourcing Self-Awareness To Artificial Intelligence",
-          category: "Technology",
-          keyword: "Military",
-        },
-        {
-          image:
-            "https://cdn-images-1.medium.com/max/1024/1*eogFpsVgNzXQLCVgFzT_-A.jpeg",
-          title: "Degrees of Freedom and Sudoko",
-          category: "Travel",
-          keyword: "Social",
-        },
-      ],
+      faq_names: [],
       search: "",
-
-      // Gauge
-      gaugeValue: 70,
-      gaugeUnit: "USD",
+      search_faq: "",
     };
+  },
+  created: function () {
+    this.getdatadb("","");
+  },
+  watch: {
+    windowWidth(newWidth, oldWidth) {
+      this.txt = `it changed to ${newWidth} from ${oldWidth}`;
+    },
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+  },
+
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+
+  methods: {
+    date(date){
+      console.log(date);
+      if(date=="Daily")
+      {
+        var fromDate = moment().format("YYYY-MM-DD");
+        var toDate = moment().add(1, 'days').startOf('day').format('YYYY-MM-DD');
+        this.getdatadb(fromDate,toDate);
+      }
+      else if(date == "Weekly")
+      {
+          fromDate = moment().subtract(7, 'days').startOf('day').format('YYYY-MM-DD');
+          toDate = moment().add(1, 'days').startOf('day').format('YYYY-MM-DD');
+          this.getdatadb(fromDate,toDate);
+      }
+       else if(date == "Monthly")
+      {
+          fromDate = moment().subtract(30, 'days').startOf('day').format('YYYY-MM-DD');
+          toDate = moment().add(1, 'days').startOf('day').format('YYYY-MM-DD');
+          this.getdatadb(fromDate,toDate);
+      }
+       else if(date == "Yearly")
+      {
+          fromDate = moment().subtract(365, 'days').startOf('day').format('YYYY-MM-DD');
+          toDate = moment().add(1, 'days').startOf('day').format('YYYY-MM-DD');
+          this.getdatadb(fromDate,toDate);
+      }
+      else{
+        this.getdatadb("",""); 
+      }
+    },
+    onResize() {
+      this.windowWidth = window.innerWidth;
+    },
+    getdatadb(from , to) {
+      var link;
+      var auth = "https://botbuilder.freshdigital.io/authentication";
+      var db = "https://botbuilder.freshdigital.io/analytics";
+      var u = this.$session_username;
+      var o = this.$session_password;
+      var token;
+      var tempfaqlist = [];
+      var self = this;
+      //cleaning
+      self.totaltime = 0;
+      self.nooffaqclicks = 0;
+      self.averageclicks = 0;
+      self.agents = 0;
+      self.website =0;
+      self.faq_names = [];
+      self.averagetime = 0;
+      //
+      if(from == "" && to == "")
+      {
+        link = db + "?username=" + u + "&password=" + o;
+      }
+      else
+      {
+        link = db + "?username=" + u + "&password=" + o + "&createdAt[$gte]=" + from+ "&createdAt[$lte]="+ to;
+      }
+      $.ajax({
+        url: auth,
+        data: {
+          strategy: "local",
+          email: "dbadmin@dwtech.in",
+          password: "admin@123",
+        },
+        type: "POST",
+      }).then(function (data) {
+        token = data.accessToken;
+        $.ajax({
+          url: link,
+          type: "GET",
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + token);
+          },
+        }).then(function (data1) {
+          if(data1){
+          for (var i = 0; i <= data1.data.length - 1; i++) {
+            self.totaltime = self.totaltime + data1.data[i].total_time;
+            self.nooffaqclicks = self.nooffaqclicks + data1.data[i].no_of_clicks;
+            self.agents = self.agents + data1.data[i].agents_clicked;
+            self.website = self.website + data1.data[i].website_clicked;
+            for (var a = 0; a <= data1.data[i].faq_clicked.length - 1; a++) {
+              tempfaqlist.push(data1.data[i].faq_clicked[a]);
+            }
+          }
+          //faq counts
+          console.log(tempfaqlist);
+          const map = tempfaqlist.reduce(
+            (acc, e) => acc.set(e, (acc.get(e) || 0) + 1),
+            new Map()
+          );
+          var faq = [],
+            counts = [];
+          faq = [...map.keys()];
+          counts = [...map.values()];
+          console.log(faq[0]);
+          for (var b = 0; b <= faq.length - 1; b++) {
+            self.faq_names.push({ name: faq[b], clicks: counts[b] });
+          }
+          self.averageclicks = self.nooffaqclicks / data1.data.length;
+          self.averagetime = self.totaltime / data1.data.length;
+          
+          }
+        });
+      });
+      
+    },
   },
   computed: {
     keywords() {
@@ -409,3 +340,6 @@ table th {
   height: 400px;
 }
 </style>
+
+
+
